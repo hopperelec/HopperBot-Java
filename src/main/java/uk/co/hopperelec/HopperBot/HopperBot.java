1ccde476a3f521e46d1e818020a1728819c6d57c
hopperelec <hopperelec@gmail.com>
2022-04-13 18:52:03 +0100

    As suggested by freya02: - Updated pom.xml and removed unused repository - Improved error handling in HopperBot.java - Separated main function into multiple logical functions

--- src/main/java/uk/co/hopperelec/HopperBot/HopperBot.java
+++ src/main/java/uk/co/hopperelec/HopperBot/HopperBot.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
@@ -29,16 +28,63 @@ public final class HopperBot {
         public HopperBotLoadingException(String message) {
             super(message);
         }
+        public HopperBotLoadingException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
+    private static String getToken() throws HopperBotLoadingException {
+        try {
+            return Files.readString(Paths.get("token"));
+        } catch (IOException e) {
+            throw new HopperBotLoadingException("'token' file must be created in the working directory",e);
+        }
+    }
+
+    private static void createConfig(Path configPath) throws HopperBotLoadingException {
+        final InputStream configStream = HopperBot.class.getResourceAsStream("/config.yml");
+        if (configStream == null) {
+            throw new HopperBotLoadingException("Failed to read default config from JAR!");
+        }
+        try {
+            Files.copy(configStream,configPath);
+        } catch (IOException e) {
+            throw new HopperBotLoadingException("Failed to save default config!",e);
+        }
+    }
+
+    private static HopperBotConfig readConfig(File configFile) throws HopperBotLoadingException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+            return objectMapper.readValue(configFile, HopperBotConfig.class);
+        } catch (IOException e) {
+            throw new HopperBotLoadingException("Failed to read config to HopperBotConfig object!",e);
+        }
+    }
+
+    private static void initializeFeatures(HopperBotConfig config, JDABuilder builder) {
+        final Set<HopperBotCommandFeature> commandFeatures = new HashSet<>();
+        for (HopperBotFeatures feature : config.getEnabledFeatures()) {
+            if (feature.handler != null) {
+                try {
+                    final HopperBotFeature featureInstance = feature.handler.newInstance();
+                    builder.addEventListeners(featureInstance);
+                    if (featureInstance instanceof HopperBotCommandFeature) {
+                        commandFeatures.add((HopperBotCommandFeature) featureInstance);
+                    }
+                } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                    logger.error("Cannot create instance of HopperBotFeature {}!",feature,e);
+                }
+            }
+        }
+        builder.addEventListeners(new HopperBotCommandHandler(commandFeatures));
     }
 
     public static void main(String[] args) {
         try {
             final JDABuilder builder = JDABuilder.create(GatewayIntent.GUILD_MEMBERS,GatewayIntent.GUILD_MESSAGES,GatewayIntent.GUILD_VOICE_STATES);
-            try {
-                builder.setToken(Files.readString(Paths.get("token")));
-            } catch (IOException e) {
-                throw new HopperBotLoadingException("'token' file must be created in the working directory");
-            }
+            builder.setToken(getToken());
             logger.info("Retrieved token");
             builder.disableCache(CacheFlag.ACTIVITY,CacheFlag.EMOTE,CacheFlag.CLIENT_STATUS,CacheFlag.ONLINE_STATUS,CacheFlag.ROLE_TAGS,CacheFlag.MEMBER_OVERRIDES);
             logger.info("Configured JDA builder");
@@ -46,61 +92,29 @@ public final class HopperBot {
             final Path configPath = Paths.get(System.getProperty("user.dir"),"config.yml");
             final File configFile = configPath.toFile();
             if (!configFile.exists() || configFile.isDirectory()) {
-                final InputStream configStream = HopperBot.class.getResourceAsStream("/config.yml");
-                if (configStream == null) {
-                    throw new HopperBotLoadingException("Failed to read default config from JAR!");
-                }
-                try {
-                    FileOutputStream outputStream = new FileOutputStream(configPath.toString());
-                    outputStream.write(configStream.readAllBytes());
-                } catch (IOException e) {
-                    throw new HopperBotLoadingException("Failed to save default config!");
-                }
+                createConfig(configPath);
                 logger.info("Created new config.yml");
             } else {
                 logger.info("Found config.yml");
             }
-
-            final HopperBotConfig config;
-            try {
-                ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-                objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-                config = objectMapper.readValue(configFile, HopperBotConfig.class);
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new HopperBotLoadingException("Failed to read config to uk.co.hopperelec.HopperBot.HopperBotConfig object!");
-            }
+            final HopperBotConfig config = readConfig(configFile);
             logger.info("Loaded config");
 
-            final Set<HopperBotCommandFeature> commandFeatures = new HashSet<>();
-            for (HopperBotFeatures feature : config.getEnabledFeatures()) {
-                if (feature.handler != null) {
-                    try {
-                        HopperBotFeature featureInstance = feature.handler.newInstance();
-                        builder.addEventListeners(featureInstance);
-                        if (featureInstance instanceof HopperBotCommandFeature) {
-                            commandFeatures.add((HopperBotCommandFeature) featureInstance);
-                        }
-                    } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
-                        logger.error("Cannot create instance of HopperBotFeature {}!",feature,e);
-                    }
-                }
-            }
-            builder.addEventListeners(new HopperBotCommandHandler(commandFeatures));
+            initializeFeatures(config,builder);
             logger.info("Done initializing features");
 
             final JDA jda;
             try {
                 jda = builder.build();
             } catch (LoginException e) {
-                throw new HopperBotLoadingException("Failed to login to the bot!");
+                throw new HopperBotLoadingException("Failed to login to the bot!",e);
             }
             HopperBotUtils.createInstance(jda,config);
             logger.info("Logged into bot");
 
         } catch (HopperBotLoadingException e) {
-            logger.error(e.getMessage());
-            System.exit(0);
+            logger.error("Error trying to load HopperBot",e);
+            System.exit(1);
         }
     }
 }
